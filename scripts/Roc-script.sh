e4cc6af8f23998be355797b78b67d0d2ebee27ca
lee29 <327208429@qq.com>
2025-08-06 22:51:29 +0800

    Update Roc-script.sh

--- scripts/Roc-script.sh
+++ scripts/Roc-script.sh
@@ -67,8 +67,8 @@ git clone --depth=1 https://github.com/NONGFAH/luci-app-athena-led package/luci-
 chmod +x package/luci-app-athena-led/root/etc/init.d/athena_led package/luci-app-athena-led/root/usr/sbin/athena-led
 
 # OpenClash（ dev 版 ）
-rm -rf feeds/luci/applications/luci-app-openclash
-git clone --depth=1 -b dev https://github.com/vernesong/OpenClash.git package/luci-app-openclash
+#rm -rf feeds/luci/applications/luci-app-openclash
+#git clone --depth=1 -b dev https://github.com/vernesong/OpenClash.git package/luci-app-openclash
 
 # iStore
 #git_sparse_clone main https://github.com/linkease/istore-ui app-store-ui
