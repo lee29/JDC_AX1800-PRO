50e673c61161faca110ef1be4d6ce10599a540af
lee29 <327208429@qq.com>
2025-08-15 00:18:04 +0800

    Update Roc-script.sh

--- scripts/Roc-script.sh
+++ scripts/Roc-script.sh
@@ -51,7 +51,6 @@ function git_sparse_clone() {
   cd .. && rm -rf $repodir
 }
 
-
 # Git稀疏克隆，只克隆指定目录到本地 ./ 目录
 function git_sparse_clone_base() {
   branch="$1" repourl="$2" && shift 2
@@ -62,7 +61,11 @@ function git_sparse_clone_base() {
   cd .. && rm -rf $repodir
 }
 
-
+# 示例: 赌徒个参数:拷贝的目的目录 后面跟的是要拷贝的源目录【可以多个】
+# source $GITHUB_WORKSPACE/functions.sh
+# merge_package master https://github.com/WYC-2020/openwrt-packages package/openwrt-packages luci-app-eqos luci-app-openclash luci-app-ddnsto ddnsto 
+# merge_package master https://github.com/lisaac/luci-app-dockerman package/lean applications/luci-app-dockerman
+#merge_package master https://github.com/coolsnowwolf/lede.git package/feeds/NueXini_Packages package/lean/vsftpd-alt
 
 
 
